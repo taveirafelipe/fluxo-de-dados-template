3bb0d80ea436cef332954286247048365b16c4c9
taveirafelipe <taveira.felipe@gmail.com>
2022-10-19 10:47:43 -0300

    Exercício aula manhã

--- src/components/FormularioCadastro/FormularioCadastro.js
+++ src/components/FormularioCadastro/FormularioCadastro.js
@@ -1,19 +1,49 @@
-import { FormContainer, Form, Input, StyledLabel } from "./styled";
+import { useState } from "react";
+import { FormContainer, Form, Input, StyledLabel, SendButton } from "./styled";
 
-export const FormularioCadastro = () => {
+export const FormularioCadastro = (props) => {
+
+const onChangeImagem = (event) =>{
+  // console.log(event.target.value)
+  props.setImagem(event.target.value)
+}
+const onChangeDescricao = (event) =>{
+  // console.log(event.target.value)
+  props.setDescricao(event.target.value)
+}
+const onChangeTitulo= (event) =>{
+  // console.log(event.target.value)
+  props.setTitulo(event.target.value)
+}
+
+const {atualiza} = props;
+
 
   return (
     <FormContainer>
       <h2>Insira sua postagem abaixo: </h2>
       <Form>
+      <StyledLabel htmlFor="foto">
+          Título:
+          <Input 
+            id="titulo" 
+            onChange={onChangeTitulo}
+            value={props.titulo}/>
+        </StyledLabel>
         <StyledLabel htmlFor="foto">
           Imagem:
-          <Input id="foto" />
+          <Input 
+            id="foto" 
+            onChange={onChangeImagem}
+            value={props.imagem}/>
         </StyledLabel>
         <StyledLabel htmlFor="descricao">
           Descrição:
-          <Input id="descricao" />
+          <Input id="descricao" 
+          onChange={onChangeDescricao}
+          value={props.descricao}/>
         </StyledLabel>
+        <SendButton onClick={atualiza} >Enviar</SendButton>
       </Form>
     </FormContainer>
   );
--- src/components/TelaDaPostagem/TelaDaPostagem.js
+++ src/components/TelaDaPostagem/TelaDaPostagem.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import {ContainerPostagem, Image, Description} from './styles'
 
-const TelaDaPostagem = () => {
+const TelaDaPostagem = (props) => {
   return (
     <ContainerPostagem>
-            <Image src={"https://picsum.photos/536/354"} alt='drone view'/>
-            <Description>{'Lorem Ipsum'}</Description>
+            <h1>{props.titulo}</h1>
+            <Image src={props.imagem} alt='drone view'/>
+            <Description>{props.descricao}</Description>
     </ContainerPostagem>
   )
 }
